e3b22f1a9b2b41eede85e639f7345d28036193cb
BasilDimopoulos <basil.dimopoulos@gmail.com>
2023-08-28 13:56:10 +0930

    added basic fetch for data and created new type for response data for improved mapping

---
+++ src/stations/station.router.ts
@@ -0,0 +1,10 @@
+import { Station } from "./types/station.interface";
+import express, { Request, Response } from "express";
+
+stationsRouter.get("/", async (req: Request, res: Response) => {
+    try{
+        const items: Station[] = await 
+    }
+})
+
+export const stationsRouter = express.Router();
---
+++ src/stations/types/station.interface.ts
@@ -0,0 +1,44 @@
+export interface Station {
+  name: string;
+  apparent_t: number;
+  lat: number;
+  lon: number;
+}
+
+export interface StationResponse {
+  sort_order?: number;
+  wmo?: number;
+  name: string;
+  history_product?: string;
+  local_date_time?: string;
+  local_date_time_full?: string;
+  aifstime_utc?: string;
+  lat: number;
+  lon: number;
+  apparent_t: number;
+  cloud?: string;
+  cloud_base_m?: string;
+  cloud_oktas?: string;
+  cloud_type?: string;
+  cloud_type_id?: string;
+  delta_t?: number;
+  gust_kmh?: number;
+  gust_kt?: number;
+  air_temp?: number;
+  dewpt?: number;
+  press?: string;
+  press_msl?: string;
+  press_qnh?: string;
+  press_tend?: string;
+  rain_trace?: number;
+  rel_hum?: number;
+  sea_state?: string;
+  swell_dir_worded?: string;
+  swell_height?: string;
+  swell_period?: string;
+  vis_km?: number;
+  weather?: string;
+  wind_dir?: string;
+  wind_spd_kmh?: number;
+  wind_spd_kt?: number;
+}
